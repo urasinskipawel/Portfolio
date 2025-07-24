44f900f27cb63ede0f9a07a94d311a982e408c30
Paweł Urasiński <uraspawel@gmail.com>
2025-07-24 12:07:28 +0200

    Made configuration of Vercel, Tailwind and TS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), react()],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+});
