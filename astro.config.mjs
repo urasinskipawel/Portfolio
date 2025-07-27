91ecf1d068ffba380ce362ba4013ec2b7335c5dc
Paweł Urasiński <uraspawel@gmail.com>
2025-07-27 20:04:37 +0200

    Added some aliases because of changing project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,8 +19,11 @@ export default defineConfig({
 			alias: {
 				'@': path.resolve('./src'),
 				'@components': path.resolve('./src/components'),
+				'@constants': path.resolve('./src/constants'),
+				'@icons': path.resolve('./src/icons'),
 				'@layouts': path.resolve('./src/layouts'),
 				'@pages': path.resolve('./src/pages'),
+				'@types': path.resolve('./src/types'),
 			},
 		},
 	},
